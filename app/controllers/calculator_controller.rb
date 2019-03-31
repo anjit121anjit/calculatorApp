140fd7a6ee62c2ce5831503dc3d2adaac002eec9
anjit121anjit <anjit11anjit@gmail.com>
2019-03-31 18:58:36 +0100

    First commit: adding all the files to the repository

---
+++ app/controllers/calculator_controller.rb
@@ -0,0 +1,20 @@
+
+
+require 'arithmetic_calculator'
+
+class CalculatorController < ApplicationController
+def calculate
+end
+
+def sum
+# the user data is sent via the params
+# :number1 key contains the first number entered
+# :number2 key contains the second number entered
+# the numbers are entered as a text
+# retrieve the number submitted by the user
+@num1 = params[:number1]
+@num2 = params[:number2]
+# we use the arithmetic_calculator.rb of the lib folder
+@result = ArithmeticCalculator.add(@num1.to_i, @num2.to_i)
+ end
+end
